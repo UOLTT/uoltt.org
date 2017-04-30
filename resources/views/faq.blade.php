52a43001640e68c4154aff63f860dafab6623b8d
Judah Wright <judah@servercanyon.com>
2017-04-30 13:16:29 -0500

    Adding a FAQ page

---
+++ resources/views/faq.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.uoltt')
+
+@section('content')
+    <h1 class="page-header">Frequently Asked Questions</h1>
+
+    <div>
+        <ul>
+            <li>
+                <p>
+                    <b>How do I update my information in the ship database?</b><br>
+
+                    Super-simple thing. Simply follow the instructions on this page:
+                    Link account to the Star Citizen user database
+                </p>
+            </li>
+            <li>
+                <p>
+                    <b>My game keeps crashing. Is there anything I can do?</b><br>
+
+                    Yes, actually. You might try updating your User.cfg file. More details can be found here:Editing Your User Config File
+                </p>
+            </li>
+            <li>
+                <p>
+                    <b>My Application to join the organization has not been accepted?</b><br>
+
+                    We are  only human, and make mistakes. If you feel there was an error, please get in touch with either Ixi or judahnator on the LTT forums.<br>
+                    We will get you sorted out. You might check to see if you were applying on the PTU site. That might be the root of the problem.
+                </p>
+            </li>
+        </ul>
+    </div>
+@endsection
