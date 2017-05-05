2b94a181737819d0a77301cb24eedfb3819a4864
Judah Wright <judah@servercanyon.com>
2017-05-05 16:15:07 -0500

    Adding a code 500 error page

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.uoltt')
+
+@section('nav-header','ERROR')
+
+@section('content')
+    <h1 class="page-header">Whoops...</h1>
+
+    <p>It looks like an error occurred. Unfortunately that's about all we know.</p>
+@endsection
