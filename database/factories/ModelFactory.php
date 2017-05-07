c12780c62f3264761229b595748b6cf7ba39f196
Judah Wright <judah@servercanyon.com>
2017-05-07 15:25:03 -0500

    Making a factory for shops

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -31,3 +31,15 @@ $factory->define(\App\Models\Commodity::class,function(Faker\Generator $faker) {
         'mass' => $faker->randomNumber()
     ];
 });
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(\App\Models\Shop::class,function(Faker\Generator $faker) {
+    return [
+        'name' => implode(' ',$faker->words(rand(1,3))),
+        'location_id' => \App\Models\Location::inRandomOrder()->whereHas('celestial_type',function(\Illuminate\Database\Eloquent\Builder $query) {
+            $query->whereNotIn('name',['Jump Point','Star','Asteriod Belt']);
+        })->first()->id,
+        'allegiance_id' => \App\Models\Allegiance::inRandomOrder()->first(['id'])->id,
+        'affiliation_id' => \App\Models\Affiliation::inRandomOrder()->first(['id'])->id
+    ];
+});
