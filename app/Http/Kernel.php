20766848d4df1d2bdaa92556d94f0f94394d1c38
Judah Wright <judah@servercanyon.com>
2017-05-13 13:30:25 -0500

    Setting up Entrust middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,9 +52,12 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'ability' => \Zizaco\Entrust\Middleware\EntrustAbility::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'permission' => \Zizaco\Entrust\Middleware\EntrustPermission::class,
+        'role' => \Zizaco\Entrust\Middleware\EntrustRole::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
 }
