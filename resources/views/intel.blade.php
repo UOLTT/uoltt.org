40be87b3e877b302fbc3e13f843be31cb8ac3a21
Judah Wright <judah@servercanyon.com>
2017-05-13 07:10:01 -0500

    Switching intel blade to the new bootstrap layout

--- resources/views/intel.blade.php
+++ resources/views/intel.blade.php
@@ -1,12 +1,14 @@
-@extends('layouts.uoltt')
+@extends('layouts.uoltt-bootstrap')
 
 @section('nav-header','INTEL')
 
 @section('content')
-    <h1 class="page-header">Intelligence System</h1>
+
+    <h1>Intelligence System</h1>
 
     <p>
         This page does not exist, nor will it ever exist.<br>
         Go away.
     </p>
+
 @endsection
