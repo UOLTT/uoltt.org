e3e0aa9cb18135d48950498bf7f93bb5a54a9670
Judah Wright <judah@servercanyon.com>
2017-05-13 14:30:26 -0500

    Adding fillable attributes and a static helper to find an element by name to the Role model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,4 +4,16 @@ namespace App\Models;
 
 use Zizaco\Entrust\EntrustRole;
 
-class Role extends EntrustRole {}
+class Role extends EntrustRole {
+
+    protected $fillable = [
+        'name',
+        'display_name',
+        'description'
+    ];
+
+    public static function findByName($name) {
+        return Role::where('name',$name)->first();
+    }
+
+}
