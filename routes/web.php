b1febda8de4b0f5c7d5986c4aaeb5e1ab10b17e2
Judah Wright <judah@servercanyon.com>
2017-06-21 16:50:49 -0500

    Removing the "/home" page since it is not really used

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,6 @@ Route::get('/commodities/report','CommoditiesController@create')
     ->name('commodities.report');
 
 Route::get('/faq','PageController@faq')->name('faq');
-Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/intel','PageController@intel')->name('intel');
 
 Route::get('/locations','LocationController@index')->name('locations.index');
